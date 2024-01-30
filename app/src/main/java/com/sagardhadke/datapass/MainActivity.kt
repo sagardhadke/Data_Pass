6176be0d8721e5f9b789a99919925b1353c7a680
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-30 23:07:55 +0530

    Pass Data to Next Activity

--- app/src/main/java/com/sagardhadke/datapass/MainActivity.kt
+++ app/src/main/java/com/sagardhadke/datapass/MainActivity.kt
@@ -6,22 +6,32 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.Switch
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image = findViewById<ImageView>(R.id.imageView)
+
         val input = findViewById<EditText>(R.id.input_et)
         val btn = findViewById<Button>(R.id.button)
+        val rate = findViewById<RatingBar>(R.id.ratingBar)
 
 
         btn.setOnClickListener {
+            val image = findViewById<ImageView>(R.id.imageView)
+            val inputData = input.text.toString()
+            val rating =  rate.rating.toFloat()
+
+            val img = R.drawable.cycle
             val i = Intent(this,GetData::class.java)
+            i.putExtra("img",img)
+            i.putExtra("input",inputData)
+            i.putExtra("rate",rating)
             startActivity(i)
-            i.putExtra("name",input.text.toString())
-            startActivity(Intent(this,GetData::class.java))
         }
 
     }
